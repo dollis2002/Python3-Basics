77dc3fc8557327914ceabc98920c42695d5f5b34
dollis2002 <59921620+dollis2002@users.noreply.github.com>
2020-11-06 16:02:35 -0600

    first commit

---
+++ code/datastructures.py
@@ -0,0 +1,17 @@
+a = 5
+b = 'hello'
+c = 3.14
+
+# Lists
+lis = [1, 2, a, 'a'] # Note that there are two a's, the first is the value assigned to the variable 'a' and the second calls string 'a'
+
+# Tuples
+tup = (11, a, b, c, 'hello')
+
+# Dictionary
+dic = {'one' : 1, 'two' : 2, 'pi' : c}
+
+# Accessing values in data structures
+lis[3] # --> 5, as 5 is stored on the variable at index 3, which is 'a'
+tup[4] # --> 'hello', value at index 4
+dic['one'] # --> 1, as it the value to the key 'one'
---
+++ code/variables.py
@@ -0,0 +1,14 @@
+a = 1 # int
+type(a) #--> class int
+
+b = 3.14 # float
+type(b) #--> class float
+
+c = 'H' # str (although it's a character, characters are strings in Python)
+type(c) #--> class str
+
+d = 'Hello world' # str (string)
+type(c) #--> class str
+
+# See how they appear
+print(a, b, c, d)
